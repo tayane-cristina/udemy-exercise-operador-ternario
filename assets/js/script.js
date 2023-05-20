8f50ee9d6e12c9fb4c021429eb63e3a584bd3acf
Tayane Cristina Alves dos Santos <tay-cristinarj@hotmail.com>
2023-05-20 10:07:10 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+//Operadores ternários = ? :
+
+//Usuários vip = a partir 1000
+//Usuários comuns = abaixo de 1000
+//condição ? valor verdadeiro : valor falso
+
+let pontuacaoUsuario = 10
+
+pontuacaoUsuario >= 1000 ? console.log("usuário vip") : console.log("Usuário comum")
+
